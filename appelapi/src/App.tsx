91a8b7881dc2e254cfad92f7eb3db202b9592ea5
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-10-15 12:09:31 +0200

    refacto en cours, repassage à un fetch

--- appelapi/src/App.tsx
+++ appelapi/src/App.tsx
@@ -10,25 +10,23 @@ interface User {
   picture: {
     medium: string;
   };
-}
-
-interface ExpandedUser {
-location: {
-  street: {
-    number: number;
-    name: string;
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: string;
   };
-  city: string;
-  state: string;
-  country: string;
-  postcode: string;
-};
-phone: string;
+  phone: string;
 }
 
 const UserGrid: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [expandedUsers, setExpandedUsers] = useState<ExpandedUser[]>([]);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
     fetchUsers();
@@ -44,50 +42,43 @@ const UserGrid: React.FC = () => {
     }
   };
 
-  useEffect(() => {
-    fetchExpandedUsers();
-  }, []);
-
-  const fetchExpandedUsers = async () => {
-    try {
-      const response = await fetch('https://randomuser.me/api/?results=10');
-      const data = await response.json();
-      setExpandedUsers(data.results);
-    } catch (error) {
-      console.error('Error fetching users:', error);
-    }
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
   };
 
   return (
     <>
-    <div>
-      {users.map((user, index) => (
-        <div key={index}>
-          <img src={user.picture.medium} alt={`${user.name.first} ${user.name.last}`} />
-          <h3>
-            {`${user.name.title} 
-            ${user.name.first} 
-            ${user.name.last}`}
-          </h3>
-          <p>{user.email}</p>
+      <div>
+        {users.map((user, index) => (
+          <div key={index}>
+            <img src={user.picture.medium} alt={`${user.name.first} ${user.name.last}`} />
+            <h3>
+              {`${user.name.title} 
+              ${user.name.first} 
+              ${user.name.last}`}
+            </h3>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
+      <button onClick={toggleExpanded}>{isExpanded ? '-' : '+'}</button>
+      {isExpanded && (
+        <div>
+          {users.map((user, index) => (
+            <div key={index}>
+              <p>Adress: {user.location.street.number}</p>
+              <p>Street: {user.location.street.name}</p>
+              <p>City: {user.location.city}</p>
+              <p>State: {user.location.state}</p>
+              <p>Country: {user.location.country}</p>
+              <p>Postcode: {user.location.postcode}</p>          
+              <p>Phone: {user.phone}</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-    <button></button>
-    <div>
-      {expandedUsers.map((expandedUser, index) => (
-        <div key={index}>
-          <p>{expandedUser.location.street.number}</p>
-          <p>{expandedUser.location.street.name}</p>
-          <p>{expandedUser.location.city}</p>
-          <p>{expandedUser.location.state}</p>
-          <p>{expandedUser.location.country}</p>
-          <p>{expandedUser.location.postcode}</p>          
-          <p>{expandedUser.phone}</p>
-        </div>
-      ))}
-    </div>
+      )}
     </>
-)};
+  );
+};
 
 export default UserGrid;
